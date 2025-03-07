737616d534b2753a55e646c026d182dfb048f801
Shehinx <166279031+Shehinx@users.noreply.github.com>
2025-03-07 15:27:19 +0530

    Create circlearea.py

---
+++ circlearea.py
@@ -0,0 +1,8 @@
+r=float(input())
+pi=3.1416
+circumference=2*pi*r
+area=pi*r*r
+z= round(circumference,2)
+x= round(area,2)
+print("The circumference of the circle is:",z)
+print("The area of the circle is:",x)
